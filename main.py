f18bc09b1c8ffc5aa982be1780f95caf734ebe3d
Karan Raj <129862220+Karan0052@users.noreply.github.com>
2024-10-07 00:55:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 st = input("Enter message")
 words = st.split(" ")
-coding = input("1 for Coding or 0 for Decoding")
+coding = input("1 for Encoding or 0 for Decoding")
 coding = True if (coding=="1") else False
 print(coding)
 if(coding):
